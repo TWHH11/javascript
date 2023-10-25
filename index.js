5455f81ebebafcd7d0a55189e43f81075cd07a99
TWHH11 <thinwuthmonehtoo@gmail.com>
2023-10-25 20:02:06 +0630

    javascript and css file added

---
+++ index.js
@@ -0,0 +1,64 @@
+/*console.log('Hello');
+console.log("its raining outside")
+window.alert("nice to meet you")
+
+//comment ;ak
+
+hik
+djfij
+*/ 
+let FirstName= 'Jhon'; //string
+let age= 20; //number
+let student= true; //booleans
+
+console.log('Hello',FirstName);
+console.log('I am',age,'years old');
+console.log('enrolled',student);
+
+document.getElementById("p1").innerHTML= 'Hello '+ FirstName
+document.getElementById("p2").innerHTML= 'I am '+ age + ' years old.'
+
+var x= 10
+
+console.log(x)
+
+const y= 989
+y=123123
+console.log(y)
+
+
+//arithmetics operators
+
+console.log(x+2);
+console.log(x-2)
+console.log(x*2)
+console.log(x/2)
+
+console.log(x%2)
+console.log(x**2)
+
+x++;
+console.log(x)
+
+x--;
+console.log(x);
+
+//if statement
+
+const yz=11
+
+if (yz==5){
+    console.log("yz is 5")
+} else if (yz==11){
+    console.log("yz is greater than 5")
+}
+
+
+//write me a program to print the sum of two numbers
+    
+
+const xy=8
+
+
+
+
